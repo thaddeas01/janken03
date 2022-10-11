ddb3135c06b5338d8acacc5e674b4d23eb02150e
thaddeas01 <shemthaddeas01@gmail.com>
2022-10-11 14:18:07 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec03/kaizi/janken03/Janken03Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.kaizi.janken03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken03Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec03/kaizi/janken03/Janken03ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.kaizi.janken03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
